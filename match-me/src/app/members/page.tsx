78d8a4d1c73115a3ee54afb4713851a12672e7b4
rundex0 <69157728+rundex0@users.noreply.github.com>
2024-11-18 18:57:06 -0500

    initial setup

---
+++ match-me/src/app/members/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export default function MembersPage() {
+    return(
+        <div>
+            <h3>
+                This will be the members page
+            </h3>
+            <Link href="/">Go back home</Link>
+        </div>
+    );
+}
---
+++ match-me/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button , Link} from "@nextui-org/react";
+import { GoSmiley } from "react-icons/go";
+
+
+export default function Home() {
+  return (
+    <div> 
+      <h1 className="text-3xl">hello</h1>
+      <Button
+       as={Link}
+       href="/members"
+       color="primary"
+       variant="bordered"
+       startContent= {<GoSmiley />}
+       >click me</Button>
+    </div>
+  );
+}
